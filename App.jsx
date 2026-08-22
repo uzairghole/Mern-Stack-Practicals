77d17ae84c22fe4746ff7a62016939f377208e91
Uzair Ghole <gholeuzair73@gmail.com>
2026-08-22 12:32:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+    return (
+        <div>
+            <Header />
+                <p>This is the main content.</p>
+        <   Footer />
+    </div>
+    );
+}
+
+export default App;
---
+++ Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+    return <h3>© 2026 MERN Lab</h3>;
+}
+
+export default Footer;
